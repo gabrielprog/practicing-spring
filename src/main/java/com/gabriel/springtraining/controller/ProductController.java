a129fddcc28c1022344862c2e4013c535182cd8f
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-10-27 19:32:10 +0000

    Alter read all to read with pagination

--- src/main/java/com/gabriel/springtraining/controller/ProductController.java
+++ src/main/java/com/gabriel/springtraining/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.gabriel.springtraining.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,14 +28,16 @@ public class ProductController {
     @Autowired
     private ProductRepo productRepo;
 
+    private final int PAGE_SIZE = 15;
+
     @PostMapping
     public @ResponseBody Product create(@RequestBody @Valid Product product) {
         return productRepo.save(product);
     }
 
     @GetMapping
-    public Iterable<Product> readAll() {
-        return productRepo.findAll();
+    public Iterable<Product> read(@RequestParam int page) {
+        return productRepo.findAll(PageRequest.of(page, PAGE_SIZE));
     }
 
     @GetMapping("/{id}")
