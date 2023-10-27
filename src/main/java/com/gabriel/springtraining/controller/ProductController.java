4ed4f84450386e06766d3f6d1de7edd6b2c98c0a
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-10-27 18:59:49 +0000

    Add find by id

--- src/main/java/com/gabriel/springtraining/controller/ProductController.java
+++ src/main/java/com/gabriel/springtraining/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.gabriel.springtraining.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,11 @@ public class ProductController {
     @GetMapping
     public Iterable<Product> readAll() {
         return productRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> readById(@PathVariable Long id) {
+        return productRepo.findById(id);
     }
 
 }
